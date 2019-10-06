c3672467101783c0b28a19d9014dbea7d54fd797
Delacour America <damerica@mi-q5.21-school.ru>
2019-10-06 20:57:16 +0300

    g

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,76 +6,112 @@
 /*   By: damerica <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 14:35:49 by damerica          #+#    #+#             */
-/*   Updated: 2019/09/30 17:07:25 by damerica         ###   ########.fr       */
+/*   Updated: 2019/10/06 20:54:38 by damerica         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#define BUF_SIZE 10
 #include "get_next_line.h"
 
-
-void ft_get_next(char **argv, const int fd, char **line)
+int ft_get_next(const int fd, char **argv, char **line)
 {
-	char *tmp;
 	int i;
+	char *s;
+	int j;
 
+	j = 0;
 	i = 0;
-	while (argv[fd][i] != '\n')
-	{
-		*line[i] = argv[fd][i];
+	while (argv[fd][j] != '\0')
+		j++;
+	while (argv[fd][i] != '\n' && argv[fd][i] != '\0')
 		i++;
+//	printf("%i eto i\n", i); 
+	if (argv[fd][i] == '\n')
+	{
+		*line = ft_strsub(argv[fd], 0, i);
+		s = ft_strsub(argv[fd], i+1, j);
+		free(argv[fd]);
+		argv[fd] = s;
+		if (argv[fd][0] == '\0') // || argv[fd][0] == '\n') // || argv[fd][1] == '\0')
+			ft_strdel(&argv[fd]);
+		//return (1);
 	}
-	tmp = argv[fd];
-	argv[fd] = ft_strdup(&argv[fd][i + 1]);
-	free(tmp);
+	else if (argv[fd][i] == '\0')
+	{
+		*line = ft_strsub(argv[fd], 0, i);
+		ft_strdel(&argv[fd]);
+	}
+	else 
+	{
+		ft_strdel(&argv[fd]);
+		return (0);
+	}
+	return (1);
 }
 
 int	get_next_line(const int fd, char **line)
 {
 	static char *argv[255];
+	char *s;
 	int ret;
-	int i;
-	char *tmp;
-	char buf[BUF_SIZE + 1];
-	int j;
+	char buf[BUFF_SIZE + 1];
 
-	j = 0;
-	i = 0;
-	//write(1, "ds", 2);
-//	if (fd < 0 || line == NULL)
-//		return (-1);
-	while ((ret = read(fd, buf, BUF_SIZE) > 0))
+	if (line == NULL || fd < 0)
+		return (-1);
+	ret = read(fd, buf, BUFF_SIZE);
+//	printf("%i ---- ret_ne\n", ret);
+	while (ret > 0)
 	{
-		write(1, "a\n", 2);
+//		printf("%i -- ret || %i -- BUFF", ret, BUFF_SIZE);
 		buf[ret] = '\0';
+	//	printf("%s - eto buf\n", buf);
 		if (argv[fd] == NULL)
 			argv[fd] = ft_strnew(1);
-		argv[fd] = ft_strjoin(argv[fd], buf);
-		printf("%s\n", argv[fd]);
+		s = ft_strjoin(argv[fd], buf);
+	//	printf("%s - eto s\n", s);
+		free(argv[fd]);
+		argv[fd] = s;
 		if (ft_strchr(argv[fd], '\n'))
-		{
-			write(1, "tyt", 3);
-			ft_get_next(argv, fd, line);
 			break;
-		}
+		ret = read(fd, buf, BUFF_SIZE);
 	}
-	//ft_get_next(argv, fd, line);
-	return (1);
+	if (ret < 0)
+		return (-1);
+	else if ((ret == 0 && argv[fd] == NULL)) // || argv[fd][0] == '\n')
+		return (0);
+	return (ft_get_next(fd, argv, line));
 }
-
+/*
 int main(int argc, char **argv)
 {
 	char *line;
-	int a;
+	int i;
 	int fd;
-	a = open(argv[1], O_RDONLY);
-	get_next_line(a, &line);
-	printf("%s", line);
+
+	if (argc == 2)
+	{
+		i = 0;
+		fd = open(argv[1], O_RDONLY);
+		while ((get_next_line(fd, &line)) == 1)
+		{
+			ft_putstr(line);
+			ft_putchar('\n');
+		}
+	}
+	close(fd);
 	return (1);
-}
+}*/
+/*
+int main(int argc, char **argv)
+{
+	char *line;
+	int i;
+	int fd;
+	if (argc == 2)
+	{
+		i = 0;
+		fd = open(argv[1], O_RDONLY);
+		printf("%i", get_next_line(fd, &line));
+	}
+	return (0);
+}*/
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,13 +6,19 @@
 /*   By: damerica <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 20:44:41 by damerica          #+#    #+#             */
-/*   Updated: 2019/09/26 20:47:54 by damerica         ###   ########.fr       */
+/*   Updated: 2019/10/06 19:35:55 by damerica         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define BUFF_SIZE 32
 #include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
